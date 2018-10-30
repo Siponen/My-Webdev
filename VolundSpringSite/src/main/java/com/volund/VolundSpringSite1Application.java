d2cdaf8c0d243c53540c654c21760d1e401b2410
Conny Siponen <c.siponen@gmail.com>
2018-10-30 17:18:24 +0100

    VolundSpringSite - Init JavaSpring Suite project
    Init JavaSpring Suite Project - We've finally found our workflow for Spring projects

---
+++ VolundSpringSite/src/main/java/com/volund/VolundSpringSite1Application.java
@@ -0,0 +1,12 @@
+package com.volund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolundSpringSite1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolundSpringSite1Application.class, args);
+	}
+}
